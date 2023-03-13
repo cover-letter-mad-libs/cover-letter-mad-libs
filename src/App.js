dda787f71eb06b9a205d4cc8c7bb771cdccdbec0
austinbhan <austinbhan@outlook.com>
2023-03-13 15:16:07 -0700

    First term functioning

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 import './App.css';
+import CoverLetter from './CoverLetter';
+import { useState } from 'react';
 
 function App() {
+  const [companyName, setCompanyName] = useState('')
+  const [userCompanyName, setUserCompanyName] = useState('')
+  
+  function handleSubmit(e) {
+    e.preventDefault();
+    setCompanyName(userCompanyName)
+    setUserCompanyName('');
+  }
+
+  console.log(userCompanyName);
+
   return (
     <div className="App">
       <header className="App-header">
       </header>
       <div className="user-inputs">
-        <p>User puts stuff here</p>
+        <form onSubmit={handleSubmit}>
+          <input 
+          placeholder='Company Name'
+          name='companyName'
+          value={userCompanyName}
+          onChange={e => setUserCompanyName(e.target.value)}/>
+          <button type="submit">Submit Me</button>
+        </form>
       </div>
       <div className="cover-letter">
-        <p>This is the cover letter</p>
+        <CoverLetter 
+        companyName = {companyName}
+        // adjective = {adjective}
+        // jobPosition = {jobPosition}
+        // place = {place}
+        />
       </div>
     </div>
   );
