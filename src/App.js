40879881d32165785fae856afc2f031af7aeb088
Brien Thomas <thomasbrien94@gmail.com>
2023-03-14 15:10:23 -0700

    multiple adjective inputs added to correlate to multiple spots in the text output

--- src/App.js
+++ src/App.js
@@ -3,25 +3,41 @@ import CoverLetter from "./CoverLetter";
 import { useState } from "react";
 
 function App() {
-  const [companyName, setCompanyName] = useState("");
+  const [companyName, setCompanyName] = useState("_____");
   const [userCompanyName, setUserCompanyName] = useState("");
 
-  const [adjective, setAdjective] = useState("");
+  const [adjective, setAdjective] = useState(['_____', '_____', '_____', '_____']);
   const [userAdjective, setUserAdjective] = useState("");
 
-  const [jobPosition, setJobPosition] = useState("");
+  const [jobPosition, setJobPosition] = useState("_____");
   const [userJobPosition, setUserJobPosition] = useState("");
 
-  const [place, setPlace] = useState("");
+  const [place, setPlace] = useState("_____");
   const [userPlace, setUserPlace] = useState("");
+
+  const [userAdjectiveTwo, setUserAdjectiveTwo] = useState("");
+  const [userAdjectiveThree, setUserAdjectiveThree] = useState("");
+  const [userAdjectiveFour, setUserAdjectiveFour] = useState("");
+
+  let adjectiveList = [];
 
   function handleSubmit(e) {
     e.preventDefault();
     setCompanyName(userCompanyName);
     setUserCompanyName("");
 
-    setAdjective(userAdjective);
+    adjectiveList.push(
+      userAdjective,
+      userAdjectiveTwo,
+      userAdjectiveThree,
+      userAdjectiveFour
+    );
+
+    setAdjective(adjectiveList);
     setUserAdjective("");
+    setUserAdjectiveTwo("");
+    setUserAdjectiveThree("");
+    setUserAdjectiveFour("");
 
     setJobPosition(userJobPosition);
     setUserJobPosition("");
@@ -29,15 +45,18 @@ function App() {
     setPlace(userPlace);
     setUserPlace("");
   }
+  console.log("adjective", adjective);
+  console.log("adjectiveList", adjectiveList);
 
   return (
     <div className="App">
+      
       <div role="header"></div>
       <div className="user-inputs">
         <form onSubmit={handleSubmit}>
           {/* Company Input */}
           <input
-            placeholder="Company Name"
+            placeholder="Someone you dislike"
             name="companyName"
             value={userCompanyName}
             onChange={(e) => setUserCompanyName(e.target.value)}
@@ -45,7 +64,7 @@ function App() {
 
           {/* Adjective Input */}
           <input
-            placeholder="How are you feeling?"
+            placeholder="One word description"
             name="adjective"
             value={userAdjective}
             onChange={(e) => setUserAdjective(e.target.value)}
@@ -61,10 +80,31 @@ function App() {
 
           {/* Job Board Input */}
           <input
-            placeholder="Job Board"
+            placeholder="A place or location"
             name="place"
             value={userPlace}
             onChange={(e) => setUserPlace(e.target.value)}
+          />
+
+          <input
+            placeholder="A descriptive word"
+            name="adjectiveTwo"
+            value={userAdjectiveTwo}
+            onChange={(e) => setUserAdjectiveTwo(e.target.value)}
+          />
+
+          <input
+            placeholder="Describe how you feel when you eat a food that should be crunchy, but is instead soft"
+            name="adjectiveThree"
+            value={userAdjectiveThree}
+            onChange={(e) => setUserAdjectiveThree(e.target.value)}
+          />
+
+          <input
+            placeholder="How do you feel when you hear the phrase 'right', or 'yeah'?"
+            name="adjectiveFour"
+            value={userAdjectiveFour}
+            onChange={(e) => setUserAdjectiveFour(e.target.value)}
           />
           <button type="submit">Submit Me</button>
         </form>
--- src/CoverLetter.js
+++ src/CoverLetter.js
@@ -4,10 +4,10 @@ export default function CoverLetter({companyName, adjective, jobPosition, place}
     return (
         <p>Dear {companyName},
 
-        I am {adjective} to apply for the position of {jobPosition}, as advertised on {place}. 
+        I am {adjective[0]} to apply for the position of {jobPosition}, as advertised on {place}. 
         Although my work experience may not be directly related to this industry, 
-        I am {adjective} in my ability to excel in this role, 
-        given my {adjective} principles and dedication to {adjective} business practices.
+        I am {adjective[1]} in my ability to excel in this role, 
+        given my {adjective[2]} principles and dedication to {adjective[3]} business practices.
         </p>
     )
 }
